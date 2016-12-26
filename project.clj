6f1ed4b23bc8ee0fbe161eb4953635e5a4a2815c
Jonathan Leonard <johanatan@gmail.com>
2016-12-25 18:30:02 -0800

    Upgrade ClojureScript+remove outdated deps.
    -. Remove test build (as it was broken and fairly useless anyway).
    -. Tweak compiler settings.

--- project.clj
+++ project.clj
@@ -1,41 +1,22 @@
 (defproject speako "0.1.0-SNAPSHOT"
   :description "GraphQL Schema Language Compiler"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :url "https://github.com/johanatan/speako"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
-                 [com.lucasbradstreet/instaparse-cljs "1.4.1.0"]
-                 [org.clojure/clojurescript "0.0-3308"]]
+                 [com.lucasbradstreet/instaparse-cljs "1.4.1.0"]]
   :node-dependencies [[source-map-support "0.2.8"]
                       [graphql "0.8.2"]]
-  :plugins [[lein-npm "0.4.0"]
-            [lein-doo "0.1.5-SNAPSHOT"]
-            [lein-cljsbuild "1.0.5"]]
-  :hooks [leiningen.cljsbuild]
-  :source-paths ["src" "target/classes"]
-  :clean-targets ["out"]
+  :plugins [[lein-npm "0.6.2"]
+            [lein-cljsbuild "1.1.4"]]
+  :clean-targets ["out" "target"]
   :cljsbuild {
     :builds {
-      :test {
-        :source-paths ["src" "test"]
-        :compiler {
-          :output-to "out/test/speako.js"
-          :output-dir "out/test"
-          :main 'speako.runner
-          :optimizations :simple
-          :pretty-print true
-          :target :nodejs
-          :hashbang false
-          :cache-analysis true
-          :source-map "out/test/speako.js.map"}}
       :main {
         :source-paths ["src"]
         :compiler {
           :output-to "out/prod/speako.js"
           :output-dir "out/prod"
           :optimizations :simple
-          :pretty-print true
-          :target :nodejs
-          :hashbang false
-          :cache-analysis true}}}
-    :test-commands {:unit ["node" :node-runner "out/test/speako.js"]}})
-
+          :target :nodejs}}}})
