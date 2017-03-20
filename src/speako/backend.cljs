34817a47c4985f4cba6cda5b419879ff0b7b631d
Jonathan Leonard <johanatan@gmail.com>
2017-03-20 12:00:35 -0700

    Store list? and required? as edge attrs.

--- src/speako/backend.cljs
+++ src/speako/backend.cljs
@@ -4,6 +4,7 @@
 (ns speako.backend
   (:require [speako.schema :as schema]
             [loom.graph :as graph]
+            [loom.attr :as attr]
             [clojure.set]))
 
 (defn- consume-types []
@@ -21,11 +22,13 @@
   (remove nil? (map (fn [[name type list? required?]]
                       (let [typ (keyword type)]
                         (when (nodes typ)
-                          [node typ]))) fields)))
+                          [node typ list? required?]))) fields)))
 
 (defn- construct-graph [parsed]
   (let [nodes (set (keys (parsed :objects)))
         unions (set (keys (parsed :unions)))
         object-edges (partial object-edges (clojure.set/union nodes unions))
-        edges (remove empty? (mapcat object-edges (parsed :objects)))]
-    (apply graph/digraph (concat edges nodes))))
+        edges (remove empty? (mapcat object-edges (parsed :objects)))
+        g (apply graph/digraph (concat (map (partial take 2) edges) nodes))
+        attrs (mapcat #(let [edge (take 2 %1)] [[edge :list? (%1 2)] [edge :required? (%1 3)]]) edges)]
+    (reduce #(attr/add-attr-to-edges %1 (%2 1) (%2 2) [(%2 0)]) g attrs)))
